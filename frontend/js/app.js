e143b4123a7b9d73254811bf5539432a485c9721
Ashutosh Teotia <rooneyteotia@gmail.com>
2022-10-03 01:11:26 +0530

    polygon test

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -228,7 +228,7 @@ async function loadInfo() {
   } else if (chain === 'polygon') {
     priceType = 'MATIC';
   }
-  const price = web3.utils.fromWei(info.deploymentConfig.mintPrice, 'milli');
+  const price = BigInt(web3.utils.fromWei(info.deploymentConfig.mintPrice, 'ether'));
   const pricePerMint = document.getElementById("pricePerMint");
   const maxPerMint = document.getElementById("maxPerMint");
   const totalSupply = document.getElementById("totalSupply");
